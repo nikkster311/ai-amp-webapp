307d29269f894ea836e38e495ec021eff12b71c3
nikkster <nikkster311@gmail.com>
2023-06-07 15:39:59 -0700

    add server, connect to server, update readme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import IndexPage from "./components/IndexPage";
 
 function App() {
-  return <IndexPage />;
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+      <IndexPage />
+    </div>
+  );
 }
 
 export default App;
