1d7b68aacc1cd0378c73735518e91a0292da1721
nikkster <nikkster311@gmail.com>
2023-06-07 18:03:08 -0700

    set up server to accept POST requests from client

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ### Run locally
 
 To start in dev mode, in the root folder, run `npm run start`.
+
 To start the server, cd into the server folder and run `npm run dev`.
 
 ### Update Github Pages
--- server/server.js
+++ server/server.js
@@ -3,11 +3,26 @@ const cors = require("cors");
 
 const app = express();
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
 app.use(cors());
 app.use(express.json());
 
+var testArr = [];
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get("/music", (req, res) => {
+  res.json("this is the music page");
+});
+
+app.post("/music", (req, res) => {
+  var test = req.body;
+  testArr.push(test);
+  res.send(test);
 });
 
 app.listen(8000, () => {
